09ab2a62928d1d263cb90c9598f72d548c493839
nicoledawn <87670272+nicoledawn92@users.noreply.github.com>
2022-10-04 16:15:15 -0700

    added further styling to form pages, messaging, error messages, about page

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Disable field_with_errors setting
+
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+    html_tag.html_safe
+  end
