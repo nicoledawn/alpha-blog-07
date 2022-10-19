40f8e2abc5ce73db8e5ef42bb15de4f423886469
nicoledawn <87670272+nicoledawn92@users.noreply.github.com>
2022-10-18 17:02:07 -0700

    added sessions, views restrictions and updated navigation

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   resources :articles
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
 
 # how to display restful routes individually
