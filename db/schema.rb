08983d3081803a4c907242e79bedf1b7b58b3b5a
nicoledawn <87670272+nicoledawn92@users.noreply.github.com>
2022-10-21 18:37:20 -0700

    add category UI views and functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_19_233629) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_21_231640) do
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
